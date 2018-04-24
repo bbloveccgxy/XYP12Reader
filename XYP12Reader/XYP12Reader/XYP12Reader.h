6c69064d18297fb30497fbcbcaf4c615b3b61a2a
gaoxinyuan <294635134@qq.com>
2018-04-24 11:53:37 +0800

    init project

---
+++ XYP12Reader/XYP12Reader/XYP12Reader.h
@@ -0,0 +1,19 @@
+//
+//  XYP12Reader.h
+//  XYP12Reader
+//
+//  Created by 高昕源 on 2018/4/24.
+//  Copyright © 2018年 高昕源. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XYP12Reader.
+FOUNDATION_EXPORT double XYP12ReaderVersionNumber;
+
+//! Project version string for XYP12Reader.
+FOUNDATION_EXPORT const unsigned char XYP12ReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYP12Reader/PublicHeader.h>
+
+
